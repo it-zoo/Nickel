5e02915585e96acdc186a179655c72765e403de2
kostya05983 <kostya05983@mail.ru>
2019-05-23 12:53:17 +0700

    [ADD] load settings from config.json resolve #8

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,8 +1,15 @@
 import DAO.messages.MessageDaoVerticle
 import controllers.RestController
+import io.vertx.core.DeploymentOptions
 import io.vertx.core.VertxOptions
+import io.vertx.core.json.JsonObject
 import io.vertx.rxjava.core.Vertx
 import org.apache.logging.log4j.LogManager
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.FileReader
+import java.nio.file.Files
+import java.nio.file.Paths
 
 fun main(args: Array<String>) {
     Server()
@@ -29,15 +36,29 @@ class Server {
     }
 
     /**
+     * Load Config from resources
+     */
+    private fun loadConfig(): JsonObject {
+        val file = this::class.java.classLoader.getResource("config.json").file
+        val br = BufferedReader(FileReader(file))
+        val joinToString = br.readLines().joinToString("")
+        br.close()
+        return JsonObject(joinToString)
+    }
+
+    /**
      * fun used to deploy vertx's verticles
      */
     private fun deployVerticles() {
+        val config = loadConfig()
         val options = VertxOptions()
+        val deploymentOptions = DeploymentOptions()
+        deploymentOptions.config = config
         Vertx.rxClusteredVertx(options).subscribe({
-            it.deployVerticle(RestController())
-            it.deployVerticle(MessageDaoVerticle())
+            it.deployVerticle(RestController(), deploymentOptions)
+            it.deployVerticle(MessageDaoVerticle(), deploymentOptions)
         }, {
-            logger.error("Whiel craeting clkustered verx", it)
+            logger.error("While creating clustered vertx", it)
         })
     }
 }
